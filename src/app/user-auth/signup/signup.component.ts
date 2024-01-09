708afad1dcc126d422695a1f295fbf564972b0c8
d3monv3num <namananand891@gmail.com>
2024-01-09 12:40:57 +0530

    Form control and validation

--- src/app/user-auth/signup/signup.component.ts
+++ src/app/user-auth/signup/signup.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators,ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 @Component({
-  selector: 'app-signup',
+  selector: 'app-root',
   standalone: true,
-  imports: [RouterLink],
+  imports: [SignupComponent,ReactiveFormsModule,RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent{
+  fb = new FormBuilder();
+  signupForm = this.fb.group({
+    businessName: new FormControl('', Validators.required),
+    userName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    mobileNumber: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    agreeTerms: new FormControl(false, Validators.requiredTrue),
+  });
 
+  onSubmit = () => {
+    if (this.signupForm.valid) {
+      // Implement your signup logic here
+      console.log('Form submitted:', this.signupForm.value);
+    }
+  };
 }
+
